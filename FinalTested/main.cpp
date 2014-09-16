f392a2430ea1012efe65af8a2e646f1b2ff19515
... <...>
2014-09-16 15:13:48 +0100

    Matrix initial check-in

--- FinalTested/main.cpp
+++ FinalTested/main.cpp
@@ -1,9 +1,13 @@
 
 #include <QtCore/QCoreApplication>
 
+#include "common-matrix4.h"
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	Common::Matrix4<float> matrix = Common::Matrix4<float>(123);
 
 	return a.exec();
 }
