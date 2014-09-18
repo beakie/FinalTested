abcfcf5fd630ae3cf91afc6f9c19c4e031d9b828
beakie <garyewen@gmail.com>
2014-09-18 19:59:55 +0100

    ...

--- FinalTested/main.cpp
+++ FinalTested/main.cpp
@@ -8,18 +8,15 @@ int main(int argc, char *argv[])
 	QCoreApplication a(argc, argv);
 
 	Common::Matrix2<float> matrix = Common::Matrix2<float>(0, 1, 2, 3);
-	
 	Common::Matrix2<float> matrix2 = Common::Matrix2<float>(matrix);
-	
 	Common::Matrix2<float> matrix3 = Common::Matrix2<float>(1);
-	
 	matrix.inverse();
-	
 	matrix2.inverse(matrix);
-	
 	float d = matrix.getDeterminant();
 	
-	Common::Matrix3<float> mmm;
+	Common::Matrix2<float> mmm2;
+	Common::Matrix3<float> mmm3;
+	Common::Matrix4<float> mmm4;
 
 	Common::MatrixAdapter2F adapter;
 	adapter.get(0, 0);
