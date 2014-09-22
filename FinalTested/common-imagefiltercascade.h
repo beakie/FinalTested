5bf9885b7c51312cf3dbea25019522118eb12b0c
... <...>
2014-09-22 15:07:01 +0100

    ...

---
+++ FinalTested/common-imagefiltercascade.h
@@ -0,0 +1,16 @@
+#ifndef IMAGEFILTERCASCADE_H
+#define IMAGEFILTERCASCADE_H
+
+#include "common-image.h"
+
+namespace Common
+{
+	class ImageFilterCascade
+	{
+		~ImageFilterCascade()
+		{
+		}
+	};
+}
+
+#endif // IMAGEFILTERCASCADE_H
