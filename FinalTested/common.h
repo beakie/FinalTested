a378100ab1613713d1c4933085e5793d0f3eb6c3
beakie <garyewen@gmail.com>
2014-09-28 18:10:51 +0100

    ...

--- FinalTested/common.h
+++ FinalTested/common.h
@@ -14,13 +14,15 @@
 
 namespace Common
 {
-
 	typedef Matrix2<bool> Matrix2B;
 	typedef Matrix3<bool> Matrix3B;
 	typedef Matrix4<bool> Matrix4B;
 	typedef Matrix2<signed char> Matrix2C;
 	typedef Matrix3<signed char> Matrix3C;
 	typedef Matrix4<signed char> Matrix4C;
+	typedef Matrix2<signed short> Matrix2S;
+	typedef Matrix3<signed short> Matrix3S;
+	typedef Matrix4<signed short> Matrix4S;
 	typedef Matrix2<signed int> Matrix2I;
 	typedef Matrix3<signed int> Matrix3I;
 	typedef Matrix4<signed int> Matrix4I;
@@ -31,28 +33,43 @@ namespace Common
 	typedef Matrix3<float> Matrix3F;
 	typedef Matrix4<float> Matrix4F;
 
-	typedef MatrixAdapter<Matrix2F, bool> MatrixAdapter2B;
-	typedef MatrixAdapter<Matrix3F, bool> MatrixAdapter3B;
-	typedef MatrixAdapter<Matrix4F, bool> MatrixAdapter4B;
-	typedef MatrixAdapter<Matrix2F, signed char> MatrixAdapter2C;
-	typedef MatrixAdapter<Matrix3F, signed char> MatrixAdapter3C;
-	typedef MatrixAdapter<Matrix4F, signed char> MatrixAdapter4C;
-	typedef MatrixAdapter<Matrix2F, signed int> MatrixAdapter2I;
-	typedef MatrixAdapter<Matrix3F, signed int> MatrixAdapter3I;
-	typedef MatrixAdapter<Matrix4F, signed int> MatrixAdapter4I;
-	typedef MatrixAdapter<Matrix2F, double> MatrixAdapter2D;
-	typedef MatrixAdapter<Matrix3F, double> MatrixAdapter3D;
-	typedef MatrixAdapter<Matrix4F, double> MatrixAdapter4D;
+	typedef MatrixAdapter<Matrix2B, bool> MatrixAdapter2B;
+	typedef MatrixAdapter<Matrix3B, bool> MatrixAdapter3B;
+	typedef MatrixAdapter<Matrix4B, bool> MatrixAdapter4B;
+	typedef MatrixAdapter<Matrix2C, signed char> MatrixAdapter2C;
+	typedef MatrixAdapter<Matrix3C, signed char> MatrixAdapter3C;
+	typedef MatrixAdapter<Matrix4C, signed char> MatrixAdapter4C;
+	typedef MatrixAdapter<Matrix2S, signed short> MatrixAdapter2S;
+	typedef MatrixAdapter<Matrix3S, signed short> MatrixAdapter3S;
+	typedef MatrixAdapter<Matrix4S, signed short> MatrixAdapter4S;
+	typedef MatrixAdapter<Matrix2I, signed int> MatrixAdapter2I;
+	typedef MatrixAdapter<Matrix3I, signed int> MatrixAdapter3I;
+	typedef MatrixAdapter<Matrix4I, signed int> MatrixAdapter4I;
+	typedef MatrixAdapter<Matrix2D, double> MatrixAdapter2D;
+	typedef MatrixAdapter<Matrix3D, double> MatrixAdapter3D;
+	typedef MatrixAdapter<Matrix4D, double> MatrixAdapter4D;
 	typedef MatrixAdapter<Matrix2F, float> MatrixAdapter2F;
 	typedef MatrixAdapter<Matrix3F, float> MatrixAdapter3F;
 	typedef MatrixAdapter<Matrix4F, float> MatrixAdapter4F;
 
 	typedef Image<bool, unsigned char> Image8B;
 	typedef Image<unsigned char, unsigned char> Image8C;
+	typedef Image<unsigned short, unsigned char> Image8S;
 	typedef Image<unsigned int, unsigned char> Image8I;
-	typedef Image<bool, unsigned int> Image16B;
-	typedef Image<unsigned char, unsigned int> Image16C;
-	typedef Image<unsigned int, unsigned int> Image16I;
+	typedef Image<bool, unsigned short> Image16B;
+	typedef Image<unsigned char, unsigned short> Image16C;
+	typedef Image<unsigned short, unsigned short> Image16S;
+	typedef Image<unsigned int, unsigned short> Image16I;
+
+	typedef RGBPixel<bool> RGBPixel2;
+	typedef RGBPixel<unsigned char> RGBPixel8;
+	typedef RGBPixel<unsigned short> RGBPixel16;
+	typedef RGBPixel<unsigned int> RGBPixel32;
+
+	typedef bool GreyPixel2;
+	typedef unsigned char GreyPixel8;
+	typedef unsigned short GreyPixel16;
+	typedef unsigned int GreyPixel32;
 
 	//typedef Plot<unsigned char> DimensionsC;
 
