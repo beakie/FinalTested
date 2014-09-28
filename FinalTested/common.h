36776bbac22a92538045bfddc0c9781f101dabdb
beakie <garyewen@gmail.com>
2014-09-28 22:53:53 +0100

    ...

--- FinalTested/common.h
+++ FinalTested/common.h
@@ -40,7 +40,6 @@ namespace Common
 	typedef Matrix2<Float> Matrix2_F;
 	typedef Matrix3<Float> Matrix3_F;
 	typedef Matrix4<Float> Matrix4_F;
-
 	typedef MatrixAdapter<Matrix2_2, UInt_2> Matrix2Adapter_2;
 	typedef MatrixAdapter<Matrix3_2, UInt_2> Matrix3Adapter_2;
 	typedef MatrixAdapter<Matrix4_2, UInt_2> Matrix4Adapter_2;
@@ -65,19 +64,15 @@ namespace Common
 	typedef MatrixAdapter<Matrix2_F, Float> Matrix2Adapter_F;
 	typedef MatrixAdapter<Matrix3_F, Float> Matrix3Adapter_F;
 	typedef MatrixAdapter<Matrix4_F, Float> Matrix4Adapter_F;
-
 	typedef RGBPixel<UInt_8> RGBPixel_8;
 	typedef RGBPixel<UInt_16> RGBPixel_16;
 	typedef RGBPixel<UInt_32> RGBPixel_32;
-
 	typedef HSVPixel<UInt_8> HSVPixel_8;
 	typedef HSVPixel<UInt_16> HSVPixel_16;
 	typedef HSVPixel<UInt_32> HSVPixel_32;
-
 	typedef UInt_8 GreyPixel_8;
 	typedef UInt_16 GreyPixel_16;
 	typedef UInt_32 GreyPixel_32;
-
 	typedef Image<UInt_2, UInt_8> Image8_2;
 	typedef Image<UInt_8, UInt_8> Image8_8;
 	typedef Image<UInt_16, UInt_8> Image8_16;
@@ -86,21 +81,18 @@ namespace Common
 	typedef Image<UInt_8, UInt_16> Image16_8;
 	typedef Image<UInt_16, UInt_16> Image16_16;
 	typedef Image<UInt_32, UInt_16> Image16_32;
-
 	typedef Image<RGBPixel_8, UInt_8> Image8RGB_8;
 	typedef Image<RGBPixel_16, UInt_8> Image8RGB_16;
 	typedef Image<RGBPixel_32, UInt_8> Image8RGB_32;
 	typedef Image<RGBPixel_8, UInt_16> Image16RGB_8;
 	typedef Image<RGBPixel_16, UInt_16> Image16RGB_16;
 	typedef Image<RGBPixel_32, UInt_16> Image16RGB_32;
-
 	typedef Image<HSVPixel_8, UInt_8> Image8HSV_8;
 	typedef Image<HSVPixel_16, UInt_8> Image8HSV_16;
 	typedef Image<HSVPixel_32, UInt_8> Image8HSV_32;
 	typedef Image<HSVPixel_8, UInt_16> Image16HSV_8;
 	typedef Image<HSVPixel_16, UInt_16> Image16HSV_16;
 	typedef Image<HSVPixel_32, UInt_16> Image16HSV_32;
-
 	typedef Image<GreyPixel_8, UInt_8> Image8Grey_8;
 	typedef Image<GreyPixel_16, UInt_8> Image8Grey_16;
 	typedef Image<GreyPixel_32, UInt_8> Image8Grey_32;
--- FinalTested/core.h
+++ FinalTested/core.h
@@ -1,13 +1,37 @@
 #ifndef CORE_H
 #define CORE_H
 
+/// <summary>
+/// Boolean (bool)
+/// </summary>
 typedef bool UInt_2;
+/// <summary>
+/// 1 byte unsigned integer (unsigned char)
+/// </summary>
 typedef unsigned char UInt_8;
+/// <summary>
+/// 2 byte unsigned integer (unsigned short)
+/// </summary>
 typedef unsigned short UInt_16;
+/// <summary>
+/// 4 byte unsigned integer (unsigned int)
+/// </summary>
 typedef unsigned int UInt_32;
+/// <summary>
+/// 1 byte signed integer (signed char)
+/// </summary>
 typedef signed char Int_8;
+/// <summary>
+/// 2 byte signed integer (signed short)
+/// </summary>
 typedef signed short Int_16;
+/// <summary>
+/// 4 byte signed integer (signed int)
+/// </summary>
 typedef signed int Int_32;
+/// <summary>
+/// Floating numeric (float)
+/// </summary>
 typedef float Float;
 
 #endif // CORE_H
