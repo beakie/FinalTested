e4bd2855866867770b6e1a77930f2b4377730dcd
beakie <garyewen@gmail.com>
2014-09-28 20:44:33 +0100

    ...

--- FinalTested/core.h
+++ FinalTested/core.h
@@ -1,1 +1,13 @@
-#include "core-types.h"
+#ifndef CORE_H
+#define CORE_H
+
+typedef bool UInt_2;
+typedef unsigned char UInt_8;
+typedef unsigned short UInt_16;
+typedef unsigned int UInt_32;
+typedef signed char Int_8;
+typedef signed short Int_16;
+typedef signed int Int_32;
+typedef float Floating;
+
+#endif // CORE_H
