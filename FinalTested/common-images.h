8295f1a29159e5bc54ea3e269e371f2d87c0d245
... <...>
2014-09-29 15:08:55 +0100

    ...

--- FinalTested/common-images.h
+++ FinalTested/common-images.h
@@ -11,15 +11,41 @@ namespace Common
 	{
 	private:
 		template <typename TIMAGE, typename TVALUE, typename TINDEX>
-		static Image<TVALUE, TINDEX> getChannelImage(const TIMAGE& image, const TINDEX channel)
+		static Image<TVALUE, TINDEX> getChannelImage(const TIMAGE& image, const TINDEX channelIndex)
 		{
 			Image<TVALUE, TINDEX> i = Image<TVALUE, TINDEX>(image.Width, image.Height);
 
 			for (TINDEX x = 0; x < image.Width; x++)
 				for (TINDEX y = 0; y < image.Height; y++)
-					i.Values[x][y] = image.Values[x][y].Channels[channel];
+					i.Values[x][y] = image.Values[x][y].Channels[channelIndex];
 
 			return i;
+		}
+
+		template <typename TIMAGE, typename TVALUE, typename TINDEX>
+		static TVALUE** getChannelPixels(const TIMAGE& image, const TINDEX channelIndex)
+		{
+			Image<TVALUE, TINDEX> i = Image<TVALUE, TINDEX>(image.Width, image.Height);
+
+			for (TINDEX x = 0; x < image.Width; x++)
+				for (TINDEX y = 0; y < image.Height; y++)
+					i.Values[x][y] = image.Values[x][y].Channels[channelIndex];
+
+			return i.Values;
+		}
+
+		template <typename TIMAGE, typename TVALUE, typename TINDEX>
+		static void setChannelPixels(const TIMAGE& image, const TINDEX channelIndex, TVALUE** channelPixels)
+		{
+			for (TINDEX x = 0; x < image.Width; x++)
+				for (TINDEX y = 0; y < image.Height; y++)
+					image.Values[x][y].Channels[channelIndex] = channelPixels[x][y];
+		}
+
+		template <typename TIMAGE, typename TVALUE, typename TINDEX>
+		static void setChannelImage(const TIMAGE& image, const TINDEX channelIndex, Image<TVALUE, TINDEX> channelImage)
+		{
+			setChannelPixels<TIMAGE, TVALUE, TINDEX>(image, channelIndex, channelImage.Values);
 		}
 
 	public:
