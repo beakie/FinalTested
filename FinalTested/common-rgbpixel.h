21dd13a6e61461c3b9bcbcf8149a99f0e42d85f9
... <...>
2014-09-29 13:06:48 +0100

    ...

---
+++ FinalTested/common-rgbpixel.h
@@ -0,0 +1,15 @@
+#ifndef COMMONRGBPIXEL_H
+#define COMMONRGBPIXEL_H
+
+namespace Common
+{
+	template <typename TVALUE>
+	struct TripleChannelPixel
+	{
+		static TVALUE getX()
+		{
+		}
+	};
+}
+
+#endif // COMMONRGBPIXEL_H
