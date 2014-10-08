6bed5c1643c2ee2476f0ca7f83c846f87327380e
... <...>
2014-10-08 16:55:21 +0100

    ...

--- FinalTested/drawing-colormapbuilders.h
+++ FinalTested/drawing-colormapbuilders.h
@@ -17,35 +17,21 @@ namespace Drawing
 		{
 			TripleColorMap<TUNITINTERVAL> map(9);
 
-			map.Values[0][0] = 0.0;
-			map.Values[0][1] = 0.0;
-			map.Values[0][2] = 0.0;
-			map.Values[0][3] = 0.0;
+			map.Values[0][0] = map.Values[0][1] = map.Values[0][2] = map.Values[0][3] = 0.0;
 			map.Values[0][4] = 0.5;
-			map.Values[0][5] = 1.0;
-			map.Values[0][6] = 1.0;
-			map.Values[0][7] = 1.0;
+			map.Values[0][5] = map.Values[0][6] = map.Values[0][7] = 1.0;
 			map.Values[0][8] = 0.5;
 
-			map.Values[1][0] = 0.0;
-			map.Values[1][1] = 0.0;
+			map.Values[1][0] = map.Values[1][1] = 0.0;
 			map.Values[1][2] = 0.5;
-			map.Values[1][3] = 1.0;
-			map.Values[1][4] = 1.0;
-			map.Values[1][5] = 1.0;
+			map.Values[1][3] = map.Values[1][4] = map.Values[1][5] = 1.0;
 			map.Values[1][6] = 0.5;
-			map.Values[1][7] = 0.0;
-			map.Values[1][8] = 0.0;
+			map.Values[1][7] = map.Values[1][8] = 0.0;
 
 			map.Values[2][0] = 0.5;
-			map.Values[2][1] = 1.0;
-			map.Values[2][2] = 1.0;
-			map.Values[2][3] = 1.0;
+			map.Values[2][1] = map.Values[2][2] = map.Values[2][3] = 1.0;
 			map.Values[2][4] = 0.5;
-			map.Values[2][5] = 0.0;
-			map.Values[2][6] = 0.0;
-			map.Values[2][7] = 0.0;
-			map.Values[2][9] = 0.0;
+			map.Values[2][5] = map.Values[2][6] = map.Values[2][7] = map.Values[2][9] = 0.0;
 
 			return map;
 		}
