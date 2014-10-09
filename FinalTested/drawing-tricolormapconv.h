5b74c0876bd59d12d11dfadc519be88c09122d7a
... <...>
2014-10-09 12:12:38 +0100

    ...

--- FinalTested/drawing-tricolormapconv.h
+++ FinalTested/drawing-tricolormapconv.h
@@ -26,14 +26,38 @@ namespace Drawing
 			if (value == 1.0) // This will ensure index is always 1 less than the highest index
 				return Drawing::TriChanPixel<TPIXEL>(_colorMap->Values[0][lastIndex], _colorMap->Values[1][lastIndex], _colorMap->Values[2][lastIndex]);
 
-			TUNITINTERVAL spacing = ((TUNITINTERVAL)1 / (lastIndex - 1));
-			UInt8 index = (UInt8)(value / spacing);
+			TUNITINTERVAL spacing = ((TUNITINTERVAL)1 / lastIndex);
+			TUNITINTERVAL indexUnrounded = value / spacing;
+			UInt8 index = (UInt8)indexUnrounded;
 
-			//TUNITINTERVAL from = _colorMap->Values[0][index];
-			//TUNITINTERVAL to = _colorMap->Values[0][index + 1];
-			//TUNITINTERVAL difference = (value - from) / (to - from);
+			if (indexUnrounded == index)
+				return Drawing::TriChanPixel<TPIXEL>(_colorMap->Values[0][index], _colorMap->Values[1][index], _colorMap->Values[2][index]);
 
-			return Drawing::TriChanPixel<TPIXEL>(value, 14, 21);
+			TUNITINTERVAL channel1;
+			TUNITINTERVAL channel2;
+			TUNITINTERVAL channel3;
+
+			TUNITINTERVAL fromIndexValue = index * spacing;
+			TUNITINTERVAL toIndexValue = (index + 1) * spacing;
+			TUNITINTERVAL valueDiff = (value - fromIndexValue) / (toIndexValue - fromIndexValue);
+
+			//// for each channel
+
+			TUNITINTERVAL fromMapping1 = _colorMap->Values[0][index];
+			TUNITINTERVAL toMapping1 = _colorMap->Values[0][index + 1];
+
+			if (fromMapping1 == toMapping1)
+				channel1 = fromMapping1;
+			else
+			{
+				TUNITINTERVAL mappingDiff1 = toMapping1 - fromMapping1;
+				TUNITINTERVAL newValue1 = (mappingDiff1 * valueDiff) + fromMapping1;
+				channel1 = newValue1;
+			}
+
+			////
+
+			return Drawing::TriChanPixel<TPIXEL>(channel1, channel2, channel3);
 		}
 	};
 
