80b6b99b05be1d2283a18da5c0347cab096de3b3
beakie <garyewen@gmail.com>
2014-10-10 03:01:21 +0100

    ...

--- FinalTested/drawing-tricolormapconv.h
+++ FinalTested/drawing-tricolormapconv.h
@@ -12,12 +12,13 @@ namespace Drawing
 	{
 	private:
 		const Drawing::TriColorMap<TUNITINTERVAL>* _colorMap; // transform this on population. lowerbound defines offset, upperbound lets calc of multiplication
-		TPIXELIN _lowerBound;
-		TPIXELIN _upperBound;
+		TPIXELIN _inLowerBound;
+		TPIXELIN _inUpperBound;
 		UInt8 _lastIndex;
 		TUNITINTERVAL _spacing;
 		TPIXELOUT _outLowerBound;
 		TPIXELOUT _outUpperBound;
+		TPIXELOUT _outBoundDiff;
 
 		//move _lastIndex, _spacing etc into here and constructor
 
@@ -25,20 +26,21 @@ namespace Drawing
 		TriColorMapConv(const Drawing::TriColorMap<TUNITINTERVAL>* colorMap, const TPIXELIN lowerBound, const TPIXELIN upperBound) //should this be input type templates?
 		{
 			_colorMap = colorMap;
-			_lowerBound = lowerBound;
-			_upperBound = upperBound;
+			_inLowerBound = lowerBound;
+			_inUpperBound = upperBound;
 			_lastIndex = _colorMap->Size - 1;
 			_spacing = ((TUNITINTERVAL)1 / _lastIndex);
 			_outLowerBound = Common::getLowerBound<TPIXELOUT>();
 			_outUpperBound = Common::getUpperBound<TPIXELOUT>();
+			_outBoundDiff = _outUpperBound - _outLowerBound;
 		}
 
 		Drawing::TriChanPixel<TPIXELOUT> convertPixel(const Float32& value)
 		{
-			if (value == _upperBound)
+			if (value == _inUpperBound)
 				return Drawing::TriChanPixel<TPIXELOUT>(_colorMap->Values[0][_lastIndex], _colorMap->Values[1][_lastIndex], _colorMap->Values[2][_lastIndex]);
 
-			TUNITINTERVAL indexUnrounded = (value - _lowerBound) / _spacing;
+			TUNITINTERVAL indexUnrounded = (value - _inLowerBound) / _spacing;
 			UInt8 index = (UInt8)indexUnrounded;
 
 			if (indexUnrounded == index)
@@ -46,7 +48,7 @@ namespace Drawing
 
 			TUNITINTERVAL fromIndexValue = index * _spacing;
 			TUNITINTERVAL toIndexValue = (index + 1) * _spacing;
-			TUNITINTERVAL valueDiff = (value - fromIndexValue - _lowerBound) / (toIndexValue - fromIndexValue - _lowerBound);
+			TUNITINTERVAL valueDiff = (value - fromIndexValue - _inLowerBound) / (toIndexValue - fromIndexValue - _inLowerBound);
 
 			TUNITINTERVAL channel[3];
 
@@ -56,12 +58,11 @@ namespace Drawing
 				TUNITINTERVAL toMapping = _colorMap->Values[i][index + 1];
 
 				if (fromMapping == toMapping)
-					channel[i] = (fromMapping * (_outUpperBound - _outLowerBound)) + _outLowerBound;
+					channel[i] = (fromMapping * _outBoundDiff) + _outLowerBound;
 				else
 				{
-					TUNITINTERVAL mappingDiff = toMapping - fromMapping;
-					TUNITINTERVAL newValue = (mappingDiff * valueDiff) + fromMapping;
-					channel[i] = (newValue * (_outUpperBound - _outLowerBound)) + _outLowerBound;
+					TUNITINTERVAL newValue = ((toMapping - fromMapping) * valueDiff) + fromMapping;
+					channel[i] = (newValue * _outBoundDiff) + _outLowerBound;
 				}
 			}
 
