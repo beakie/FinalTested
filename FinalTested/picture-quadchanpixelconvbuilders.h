5c40febb2ece330dabd0195bb9d7702bb5a5889d
beakie <garyewen@gmail.com>
2014-10-12 21:36:59 +0100

    ...

--- FinalTested/picture-quadchanpixelconvbuilders.h
+++ FinalTested/picture-quadchanpixelconvbuilders.h
@@ -1,13 +1,60 @@
-// ***********************************
-// QuadChanPixelConvBuilders
-// ***********************************
-//
-//getSpread0QuadChanPixelConv
-//getSpread1QuadChanPixelConv
-//getSpread2QuadChanPixelConv
-//getSpread3QuadChanPixelConv
-//
-//getSelect0QuadChanPixelConv
-//getSelect1QuadChanPixelConv
-//getSelect2QuadChanPixelConv
-//getSelect3QuadChanPixelConv
+#ifndef PICTUREQUADCHANPIXELCONVBUILDERSCONV_H
+#define PICTUREQUADCHANPIXELCONVBUILDERSCONV_H
+
+#include "picture-quadchanpixel.h"
+#include "picture-spreadchanquadchanpixelconv.h"
+#include "picture-selectchanquadchanpixelconv.h"
+
+namespace Picture
+{
+	template <typename TVALUE>
+	SpreadChanQuadChanPixelConv<TVALUE> getSpread0QuadChanPixelConv()
+	{
+		return SpreadChanQuadChanPixelConv<TVALUE>(0);
+	}
+
+	template <typename TVALUE>
+	SpreadChanQuadChanPixelConv<TVALUE> getSpread1QuadChanPixelConv()
+	{
+		return SpreadChanQuadChanPixelConv<TVALUE>(1);
+	}
+
+	template <typename TVALUE>
+	SpreadChanQuadChanPixelConv<TVALUE> getSpread2QuadChanPixelConv()
+	{
+		return SpreadChanQuadChanPixelConv<TVALUE>(2);
+	}
+
+	template <typename TVALUE>
+	SpreadChanQuadChanPixelConv<TVALUE> getSpread3QuadChanPixelConv()
+	{
+		return SpreadChanQuadChanPixelConv<TVALUE>(3);
+	}
+
+	template <typename TVALUE>
+	SelectChanQuadChanPixelConv<TVALUE> getSelect0QuadChanPixelConv()
+	{
+		return SelectChanQuadChanPixelConv<TVALUE>(0);
+	}
+
+	template <typename TVALUE>
+	SelectChanQuadChanPixelConv<TVALUE> getSelect1QuadChanPixelConv()
+	{
+		return SelectChanQuadChanPixelConv<TVALUE>(1);
+	}
+
+	template <typename TVALUE>
+	SelectChanQuadChanPixelConv<TVALUE> getSelect2QuadChanPixelConv()
+	{
+		return SelectChanQuadChanPixelConv<TVALUE>(2);
+	}
+
+	template <typename TVALUE>
+	SelectChanQuadChanPixelConv<TVALUE> getSelect3QuadChanPixelConv()
+	{
+		return SelectChanQuadChanPixelConv<TVALUE>(3);
+	}
+
+}
+
+#endif // PICTUREQUADCHANPIXELCONVBUILDERSCONV_H
