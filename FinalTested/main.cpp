f99b190519a87daed3bd8dc07daa2a07b2018d28
... <...>
2014-10-14 12:10:44 +0100

    ...

--- FinalTested/main.cpp
+++ FinalTested/main.cpp
@@ -34,8 +34,8 @@ int main(int argc, char *argv[])
 	//Common::Image16U8 testImageb = Common::Image16U8(10, 10, 0); // Errors
 	Common::Image8F32 testImage = Common::Image8F32(100, 100, 0.51);
 
-	//Picture::TriColorMap32 map = Picture::getRGBJetColorMap<Common::UnitInterval32>();
-	Picture::TriColorMap32 map = Picture::getRGBGreyColorMap<Common::UnitInterval32>();
+	Picture::TriColorMap32 map = Picture::getRGBJetColorMap<Common::UnitInterval32>();
+	//Picture::TriColorMap32 map = Picture::getRGBGreyColorMap<Common::UnitInterval32>();
 	//Picture::Image8RGBPixel8 testImageDisk = Picture::Qt::getRGBImage<UInt8>(QImage("c:\\Test8.jpg"))
 	//													 .getImage<UInt8>(&Picture::getSelect0TriChanPixelConv<UInt8>())
 	//													 .getImage<Float32>(&Common::ScaleCastPixelConv<UInt8, Float32>())
@@ -47,15 +47,18 @@ int main(int argc, char *argv[])
 	//Picture::Image8RGBPixel8 testImageDisk = Common::Image<Int8, UInt8>(10, 10, 0.1)
 	//													.getImage<Picture::RGBPixel8>(&Picture::TriColorMapConv<Common::UnitInterval32, Int8, UInt8>(&map));
 
-	Picture::Image8RGBPixel8 testImageDisk = Picture::Qt::getRGBImage<UInt8>(QImage("c:\\Test8.jpg")) //(QImage("c:\\Grey.jpg"))
+	Picture::Image8RGBPixel8 testImageDisk = Picture::Qt::getRGBImage<UInt8>(QImage("c:\\Grey.jpg")) //(QImage("c:\\Test8.jpg"))
 																.getImage<UInt8>(&Picture::getSelect0TriChanPixelConv<UInt8>())
 																.getImage<Picture::RGBPixel8>(&Picture::TriColorMapConv<Common::UnitInterval32, UInt8, UInt8>(&map));
 
-	QWidget widget;
-	QLabel* label = new QLabel(&widget);
-	label->setPixmap(Picture::Qt::getQPixmap<UInt8>(testImageDisk));
-	label->show();
-	widget.show();
+	if (false)
+	{
+		QWidget widget;
+		QLabel* label = new QLabel(&widget);
+		label->setPixmap(Picture::Qt::getQPixmap<UInt8>(testImageDisk));
+		label->show();
+		widget.show();
 
-	return a.exec();
+		return a.exec();
+	}
 }
