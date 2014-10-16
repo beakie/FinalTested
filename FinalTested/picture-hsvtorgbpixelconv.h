62c889d89aacb88a5eeffa0d4de94576bba6e93f
... <...>
2014-10-16 11:39:55 +0100

    ...

---
+++ FinalTested/picture-hsvtorgbpixelconv.h
@@ -0,0 +1,22 @@
+#ifndef PICTUREHSVTORGBPIXELCONV_H
+#define PICTUREHSVTORGBPIXELCONV_H
+
+#include "core.h"
+#include "common.h"
+#include "picture-trichanpixel.h"
+
+namespace Picture
+{
+	template <typename TVALUE>
+	class HsvToRgbPixelConv
+	{
+	public:
+		Picture::TriChanPixel<TVALUE> convertPixel(const Picture::TriChanPixel<TVALUE>& pixel)
+		{
+
+		}
+	};
+
+}
+
+#endif // PICTUREHSVTORGBPIXELCONV_H
