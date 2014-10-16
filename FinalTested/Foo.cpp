145a23f0fe4ec3e224b71549b6fb2b201036dc0b
... <...>
2014-10-16 11:41:58 +0100

    ...

---
+++ FinalTested/Foo.cpp
@@ -0,0 +1,8 @@
+/*
+
+1) Do Hsv to Rgb and vice versa
+2) Migrate tri and quad pixel to vector?
+3) Do matrix based color model converters
+3) Replicate (and check existing) quad code from tri code
+
+*/
