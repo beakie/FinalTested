756fb04555cd0a83497b2f5ff08e4934b2aae3ca
... <...>
2014-10-16 11:33:33 +0100

    ...

---
+++ FinalTested/picture-quadcolormappixelconv.h
@@ -0,0 +1,17 @@
+#ifndef PICTUREQUADCOLORMAPCONV_H
+#define PICTUREQUADCOLORMAPCONV_H
+
+#include "core.h"
+#include "common.h"
+#include "picture-quadchanpixel.h"
+
+namespace Picture
+{
+	template <typename TUNITINTERVAL, typename TPIXELIN, typename TPIXELOUT>
+	class QuadColorMapConv
+	{
+	};
+
+}
+
+#endif // PICTUREQUADCOLORMAPCONV_H
