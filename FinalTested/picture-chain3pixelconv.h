5a3977b3335192afccd7d85edc46682dba3edef0
... <...>
2014-10-16 13:20:37 +0100

    ...

--- FinalTested/picture-chain3pixelconv.h
+++ FinalTested/picture-chain3pixelconv.h
@@ -7,6 +7,26 @@
 
 namespace Picture
 {
+	template <typename TVALUEOUT, typename TCONVERTER1, typename TCONVERTER2, typename TVALUEIN>
+	class Chain2PixelConv
+	{
+	private:
+		TCONVERTER1* _Converter1;
+		TCONVERTER2* _Converter2;
+
+		Chain2PixelConv(TCONVERTER1* converter1, TCONVERTER2* converter2)
+		{
+			_Converter1 = converter1;
+			_Converter2 = converter2;
+		}
+
+	public:
+		TVALUEOUT convertPixel(const TVALUEIN& pixel)
+		{
+			return _Converter2->convertPixel(_Converter1->convertPixel(pixel));
+		}
+	};
+
 	template <typename TVALUEOUT, typename TCONVERTER1, typename TCONVERTER2, typename TCONVERTER3, typename TVALUEIN>
 	class Chain3PixelConv
 	{
@@ -26,6 +46,30 @@ namespace Picture
 		TVALUEOUT convertPixel(const TVALUEIN& pixel)
 		{
 			return _Converter3->convertPixel(_Converter2->convertPixel(_Converter1->convertPixel(pixel)));
+		}
+	};
+
+	template <typename TVALUEOUT, typename TCONVERTER1, typename TCONVERTER2, typename TCONVERTER3, typename TCONVERTER4, typename TVALUEIN>
+	class Chain4PixelConv
+	{
+	private:
+		TCONVERTER1* _Converter1;
+		TCONVERTER2* _Converter2;
+		TCONVERTER3* _Converter3;
+		TCONVERTER4* _Converter4;
+
+		Chain4PixelConv(TCONVERTER1* converter1, TCONVERTER2* converter2, TCONVERTER3* converter3, TCONVERTER4* converter4)
+		{
+			_Converter1 = converter1;
+			_Converter2 = converter2;
+			_Converter3 = converter3;
+			_Converter4 = converter4;
+		}
+
+	public:
+		TVALUEOUT convertPixel(const TVALUEIN& pixel)
+		{
+			return _Converter4->convertPixel(_Converter3->convertPixel(_Converter2->convertPixel(_Converter1->convertPixel(pixel))));
 		}
 	};
 
