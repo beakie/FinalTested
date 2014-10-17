97cfac98fdec7626d8e1d30db883a200b043915a
... <...>
2014-10-17 14:26:02 +0100

    ...

--- FinalTested/picture-hsvtorgbpixelconv.h
+++ FinalTested/picture-hsvtorgbpixelconv.h
@@ -9,15 +9,37 @@ namespace Picture
 	template <typename TVALUE>
 	class HsvToRgbPixelConv
 	{
+		// move function variables to here
+
 	public:
 		Common::Vector3<TVALUE> convertPixel(const Common::Vector3<TVALUE>& pixel)
 		{
+			if (pixel.Values[1] == 0)
+				return Common::Vector3<TVALUE>(pixel.Values[2], pixel.Values[2], pixel.Values[2]);
 
+			float h = pixel.Values[0] / 60;
+			int i = floor(h);
+			float f = h - i;
+			float p = pixel.Values[2] * (1 - pixel.Values[1]);
+			float q = pixel.Values[2] * (1 - pixel.Values[1] * f);
+			float t = pixel.Values[2] * (1 - pixel.Values[1] * (1 - f));
+			switch (i) {
+			case 0:
+				return Common::Vector3<TVALUE>(v, t, p);
+			case 1:
+				return Common::Vector3<TVALUE>(q, v, p);
+			case 2:
+				return Common::Vector3<TVALUE>(p, v, t);
+			case 3:
+				return Common::Vector3<TVALUE>(p, q, v);
+			case 4:
+				return Common::Vector3<TVALUE>(t, p, v);
+			default:
+				return Common::Vector3<TVALUE>(v, p, q);
+			}
 		}
 	};
 
 }
 
 #endif // PICTUREHSVTORGBPIXELCONV_H
-
-//http://www.cs.rit.edu/~ncs/color/t_convert.html#RGB%20to%20HSV%20&%20HSV%20to%20RGB
--- FinalTested/picture-rgbtohsvpixelconv.h
+++ FinalTested/picture-rgbtohsvpixelconv.h
@@ -9,10 +9,34 @@ namespace Picture
 	template <typename TVALUE>
 	class RgbToHsvPixelConv
 	{
+		// move function variables to here
+
 	public:
 		Common::Vector3<TVALUE> convertPixel(const Common::Vector3<TVALUE>& pixel)
 		{
+			float h, s, v;
 
+			float minChannel = Common::min(pixel.Values[0], pixel.Values[1], pixel.Values[2]);
+			float maxChannel = Common::max(pixel.Values[0], pixel.Values[1], pixel.Values[2]);
+			v = maxChannel;				// v
+			float delta = maxChannel - minChannel;
+			if (maxChannel != 0)
+				s = delta / maxChannel;		// s
+			else {
+				// r = g = b = 0		// s = 0, v is undefined
+				s = 0;
+				h = -1;
+				return;
+			}
+			if (r == maxChannel)
+				h = (g - b) / delta;		// between yellow & magenta
+			else if (g == maxChannel)
+				h = 2 + (b - r) / delta;	// between cyan & yellow
+			else
+				*h = 4 + (r - g) / delta;	// between magenta & cyan
+			h *= 60;				// degrees
+			if (h < 0)
+				h += 360;
 		}
 	};
 
