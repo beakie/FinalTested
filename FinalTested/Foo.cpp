373d1f7044fe3154d5526ed2ef8625744c88d38b
... <...>
2014-10-17 16:17:05 +0100

    .

--- FinalTested/Foo.cpp
+++ FinalTested/Foo.cpp
@@ -5,5 +5,6 @@
 3) Do matrix based color model converters
 4) Write color model pixelconv for all models which use matrix multiplication
 5) Change pixel to color for colormodel stuff?
+6) Create color convert functions (and call them from the pixel convert code?)
 
 */
