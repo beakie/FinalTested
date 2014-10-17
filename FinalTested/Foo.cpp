1e19919422ab9ab0f251f028fc61017db318e71a
... <...>
2014-10-17 15:33:05 +0100

    removed matrixadapter

--- FinalTested/Foo.cpp
+++ FinalTested/Foo.cpp
@@ -3,6 +3,7 @@
 1) Do Hsv to Rgb and vice versa
 2) Migrate tri and quad pixel to vector?
 3) Do matrix based color model converters
-1) write color model pixelconv for all models which use matrix multiplication
+4) write color model pixelconv for all models which use matrix multiplication
+5) Remove matrixadapter and matrixn?
 
 */
