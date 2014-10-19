d38eb1f837ccddaf010c48eda3b281236aee8fbe
beakie <garyewen@gmail.com>
2014-10-19 23:40:07 +0100

    ...

--- FinalTested/Foo.cpp
+++ FinalTested/Foo.cpp
@@ -1,13 +1,14 @@
 /*
 
-1) Do Hsv to Rgb and vice versa
-2) Migrate tri and quad pixel to vector?
-3) Do matrix based color model converters
-4) Write color model pixelconv for all models which use matrix multiplication
-5) Change pixel to color for colormodel stuff?
-6) Create color convert functions (and call them from the pixel convert code?)
-7) Make UnitInterval class instead of float... should do a mod 1 on assignment operator
-8) Change colormap stuff to use vector3
-9) Rename colormap linearcolormap?
+ 1) Do Hsv to Rgb and vice versa
+ 2) Migrate tri and quad pixel to vector?
+ 3) Do matrix based color model converters
+ 4) Write color model pixelconv for all models which use matrix multiplication
+ 5) Change pixel to color for colormodel stuff?
+ 6) Create color convert functions (and call them from the pixel convert code?)
+ 7) Make UnitInterval class instead of float... should do a mod 1 on assignment operator
+ 8) Change colormap stuff to use vector3
+ 9) Rename colormap linearcolormap?
+10) Maybe make a unit interval wrapper class that could be put around a basic data float variable
 
 */
