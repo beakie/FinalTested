42e8a2362865af67e521bb4cb9037ee2109b154c
... <...>
2014-10-20 16:36:58 +0100

    ...

--- FinalTested/picture-tricolormappalettepixelconv.h
+++ FinalTested/picture-tricolormappalettepixelconv.h
@@ -13,36 +13,24 @@ namespace Picture
 	{
 	private:
 		const TriColorMap<TUNITINTERVAL>* _colorMap; // transform this on population. lowerbound defines offset, upperbound lets calc of multiplication
-		FloatMax _inLowerBound;
-		FloatMax _inUpperBound;
-		FloatMax _inBoundDiff;
 		FloatMax _outLowerBound;
 		FloatMax _outUpperBound;
 		FloatMax _outBoundDiff;
-		FloatMax _channel[3]; // this should be color not [3]
 
 	public:
-		TriColorMapPalettePixelConv(const TriColorMap<TUNITINTERVAL>* colorMap, const TPIXELIN lowerBound, const TPIXELIN upperBound) //should this be input type templates?
+		TriColorMapPalettePixelConv(const TriColorMap<TUNITINTERVAL>* colorMap)
 		{
 			_colorMap = colorMap;
-			_inLowerBound = lowerBound;
-			_inUpperBound = upperBound;
-			_inBoundDiff = upperBound - lowerBound;
 			_outLowerBound = Common::getLowerBound<TPIXELOUT>();
 			_outUpperBound = Common::getUpperBound<TPIXELOUT>();
 			_outBoundDiff = _outUpperBound - _outLowerBound;
 		}
 
-		TriColorMapPalettePixelConv(const TriColorMap<TUNITINTERVAL>* colorMap)
-			: TriColorMapPalettePixelConv(colorMap, Common::getLowerBound<TPIXELIN>(), Common::getUpperBound<TPIXELIN>())
-		{
-		}
-
 		Common::Vector3<TPIXELOUT> convertPixel(const TPIXELIN& value)
 		{
-			//return Common::Vector3<TPIXELOUT>((_channel[0] * _outBoundDiff) + _outLowerBound,
-			//	(_channel[1] * _outBoundDiff) + _outLowerBound,
-			//	(_channel[2] * _outBoundDiff) + _outLowerBound);
+			return Common::Vector3<TPIXELOUT>((_colorMap[value].Values[0] *_outBoundDiff) + _outLowerBound,
+												(_colorMap[value].Values[1] * _outBoundDiff) + _outLowerBound,
+												(_colorMap[value].Values[2] * _outBoundDiff) + _outLowerBound);
 		}
 	};
 
