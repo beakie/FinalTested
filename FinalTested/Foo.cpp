b73c7a6d5de69db4a9d374673a9eb139c567a5b5
... <...>
2014-10-21 16:40:37 +0100

    ...

--- FinalTested/Foo.cpp
+++ FinalTested/Foo.cpp
@@ -14,5 +14,6 @@
 12) Do some arduino tests
 13) Does the first colormapper code really need bounds? Bounds are only needed with floats (?) will i be doing this with floats? Instead make a seperate converter just for floats. Will make c/m faster for non float.
 14) Migrate list Items stuff into small methods like image does?
+15) Should the color stuff of picture actually be in common? .NET keeps it in system.drawing... do i?
 
 */
