0af3ab4876730c89caa64b3358aa535be2a9c0cb
... <...>
2014-10-30 10:35:13 +0000

    ...

--- FinalTested/movement-iactuator.h
+++ FinalTested/movement-iactuator.h
@@ -11,6 +11,8 @@ namespace Movement
 	{
 		Common::Matrix4<TVALUE> getTransformMatrix();
 
+		// add set(TVALUE) function? would be good to have a single point where a float can be passed to an actuator. will this work for all?
+
 		virtual ~IActuator()
 		{
 		}
