99a7616cfb97598c3463e23b843da461edd100f8
... <...>
2014-10-30 09:08:47 +0000

    ...

--- FinalTested/movement-bone.h
+++ FinalTested/movement-bone.h
@@ -5,10 +5,6 @@
 #include "common.h"
 #include "space3d.h"
 
-// THINK LOTS ABOUT GRIPPERS. GREAT EXAMPLE TO GET DESIGN RIGHT. HAS ONE ACTUATOR AND MULTIPLE JOINTS.
-// http://www.robotforstudents.com/images_gripper/grip_hand6.png
-// Made up or a simple rotational joint with a more complication joint which is the result of geom of a fixed point and the moving point
-
 namespace Movement
 {
 	template <typename TVALUE>
@@ -18,7 +14,6 @@ namespace Movement
 		UInt8 ChildrenBoneCount;
 		Bone<TVALUE>** ChildrenBones;
 		Common::Matrix4<TVALUE> JointTransformMatrix;
-		Common::Matrix4<TVALUE> BoneTransformMatrix; //should this be here??? think long and hard. is this a job of the actuator? makes sense for some servos, but for grippers etc?
 
 		Bone()
 			: ParentBone(nullptr),
@@ -42,9 +37,9 @@ namespace Movement
 		Common::Matrix4<TVALUE> getArmTransformMatrix()
 		{
 			if (ParentBone == nullptr)
-				return JointTransformMatrix * BoneTransformMatrix;
+				return JointTransformMatrix;
 			else
-				return ParentBone->getArmTransformMatrix() * JointTransformMatrix * BoneTransformMatrix;
+				return ParentBone->getArmTransformMatrix() * JointTransformMatrix;
 		}
 
 		Bone<TVALUE>& addBone()
