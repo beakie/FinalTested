a99dca6fa17a61b91fb100f40429d8a0eb240ea2
... <...>
2014-10-31 14:12:15 +0000

    ...

--- FinalTested/main.cpp
+++ FinalTested/main.cpp
@@ -67,11 +67,10 @@ int main(int argc, char *argv[])
 
 	*/
 
-	Movement::JointList<> jointList = Movement::JointList<>(); // build this into skeletal?
 	Movement::Skeletal<> skeletal;
-	skeletal.addArm(&jointList.addJoint())
-			.addBone(&jointList.addJoint())
-			.addBone(&jointList.addJoint());
+	skeletal.addArm(&skeletal.addJoint())
+			.addBone(&skeletal.addJoint())
+			.addBone(&skeletal.addJoint());
 	Movement::ActuatorManager64 actuatorManager = Movement::ActuatorManager64();
 	actuatorManager.addActuator(Movement::RotatingActuator64(Common::Vector3F(0.0, 0.0, 0.0), Common::Vector3F(10.0, 10.0, 10.0)));
 	actuatorManager.addBoneLink(actuatorManager.Actuators[0], skeletal.Arms[0]);
