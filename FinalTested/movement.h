5af3ccd2a2fc61e337fe738f04c21ca2f503244b
... <...>
2014-10-31 10:37:19 +0000

    ...

--- FinalTested/movement.h
+++ FinalTested/movement.h
@@ -10,6 +10,30 @@
 
 namespace Movement
 {
+	typedef ActuatorManager<Float32, Common::UnitInterval32> ActuatorManager32;
+
+	typedef ActuatorManager<Float64, Common::UnitInterval64> ActuatorManager64;
+
+	typedef ActuatorManager<FloatMax, Common::UnitIntervalMax> ActuatorManagerF;
+
+	typedef LinearActuator<Float32, Common::UnitInterval32> LinearActuator32;
+
+	typedef LinearActuator<Float64, Common::UnitInterval64> LinearActuator64;
+
+	typedef LinearActuator<FloatMax, Common::UnitIntervalMax> LinearActuatorF;
+
+	typedef RotatingActuator<Float32, Common::UnitInterval32> RotatingActuator32;
+
+	typedef RotatingActuator<Float64, Common::UnitInterval64> RotatingActuator64;
+
+	typedef RotatingActuator<FloatMax, Common::UnitIntervalMax> RotatingActuatorF;
+
+	typedef Skeletal<Float32> Skeletal32;
+
+	typedef Skeletal<Float64> Skeletal64;
+
+	typedef Skeletal<FloatMax> SkeletalF;
+
 }
 
 #endif // MOVEMENT_H
