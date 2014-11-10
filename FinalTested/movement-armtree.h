cb1368438b78e8c78ca6300573aa54f9ae48dd4b
... <...>
2014-11-10 14:27:49 +0000

    .

---
+++ FinalTested/movement-armtree.h
@@ -0,0 +1,77 @@
+#ifndef MOVEMENTBONE_H
+#define MOVEMENTBONE_H
+
+#include "core.h"
+#include "common.h"
+#include "space3d.h"
+
+#include "movement-joint.h"
+
+namespace Movement
+{
+	template <typename TVALUE = FloatMax>
+	struct Bone
+	{
+		Bone<TVALUE>* ParentBone;
+		Bone<TVALUE>** ChildrenBones;
+		UInt8 ChildrenBoneCount;
+		Joint<TVALUE>* Joint_;
+
+		Bone(Joint<TVALUE>* joint)
+			: ParentBone(nullptr),
+			ChildrenBoneCount(0),
+			ChildrenBones(0),
+			Joint_(joint)
+		{
+		}
+
+		Bone(Bone<TVALUE>* parentBone, Joint<TVALUE>* joint)
+			: ParentBone(parentBone),
+			ChildrenBoneCount(0),
+			ChildrenBones(0),
+			Joint_(joint)
+		{
+		}
+
+		Bone(const Bone<TVALUE>& bone)
+		{
+			// todo
+		}
+
+		Common::Matrix4<TVALUE> getArmTransformMatrix()
+		{
+			if (ParentBone == nullptr)
+				return *Joint;
+			else
+				return ParentBone->getArmTransformMatrix() * *Joint_;
+		}
+
+		Bone<TVALUE>& addBone(Joint<TVALUE>* joint)
+		{
+			Bone<TVALUE>** tmpChildrenBones = new Bone<TVALUE>*[ChildrenBoneCount + 1];
+
+			for (UInt8 i = 0; i < ChildrenBoneCount; i++)
+				tmpChildrenBones[i] = ChildrenBones[i];
+
+			tmpChildrenBones[ChildrenBoneCount] = new Bone<TVALUE>(this, joint);
+
+			delete[] ChildrenBones;
+
+			ChildrenBones = tmpChildrenBones;
+
+			ChildrenBoneCount++;
+
+			return *ChildrenBones[ChildrenBoneCount - 1];
+		}
+
+		~Bone()
+		{
+			for (UInt8 i = 0; i < ChildrenBoneCount; i++)
+				delete ChildrenBones[i];
+
+			delete[] ChildrenBones;
+		}
+	};
+}
+
+#endif // MOVEMENTBONE_H
--- FinalTested/movement.h
+++ FinalTested/movement.h
@@ -1,6 +1,7 @@
 #ifndef MOVEMENT_H
 #define MOVEMENT_H
 
+#include "movement-bone.h"
 #include "movement-jointlist.h"
 
 namespace Movement
