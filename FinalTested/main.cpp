6251c1c7c4263452c5ce07341d9be0cc17cce5c6
... <...>
2014-11-14 11:22:50 +0000

    ...

--- FinalTested/main.cpp
+++ FinalTested/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char *argv[])
 
 	Movement::Skeletal3d skeletal = Movement::Skeletal3d(4);
 
-	Movement::getTranslatedBones3d<FloatMax>(skeletal.Joints, skeletal.BoneMap, skeletal.ZeroPositions);
+	Movement::getTranslatedBones3d(skeletal.Joints, skeletal.BoneMap, skeletal.ZeroPositions);
 
 	return a.exec();
 
