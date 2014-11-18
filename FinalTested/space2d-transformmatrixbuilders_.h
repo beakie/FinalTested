50fcbcfa39c2db5b2806a8af36c3bb1b18445771
... <...>
2014-11-18 09:22:03 +0000

    ...

--- FinalTested/space2d-transformmatrixbuilders_.h
+++ FinalTested/space2d-transformmatrixbuilders_.h
@@ -6,7 +6,7 @@
 namespace Space2d
 {
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getTranslationMatrix(const TVALUE x, const TVALUE y)
+	Common::Matrix3<TVALUE> getTranslation2dMatrix(const TVALUE x, const TVALUE y)
 	{
 		return Common::Matrix3<TVALUE>(  1.0,  0.0,    x,
 										 0.0,  1.0,    y,
@@ -14,13 +14,13 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getTranslationMatrix(const Common::Vector2<TVALUE>& plot)
+	Common::Matrix3<TVALUE> getTranslation2dMatrix(const Common::Vector2<TVALUE>& plot)
 	{
 		return getTranslationMatrix<TVALUE>(plot.Values[0], plot.Values[1]);
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getScalingMatrix(const TVALUE x, const TVALUE y)
+	Common::Matrix3<TVALUE> getScaling2dMatrix(const TVALUE x, const TVALUE y)
 	{
 		return Common::Matrix3<TVALUE>(    x,  0.0,  0.0,
 										 0.0,    y,  0.0,
@@ -28,7 +28,7 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getRotationMatrix(const TVALUE rotation)
+	Common::Matrix3<TVALUE> getRotation2dMatrix(const TVALUE rotation)
 	{
 		TVALUE c = Common::cos<TVALUE>(rotation);
 		TVALUE s = Common::sin<TVALUE>(rotation);
@@ -39,19 +39,19 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getRotationMatrix(const TVALUE rotation, const TVALUE x, const TVALUE y)
+	Common::Matrix3<TVALUE> getRotation2dMatrix(const TVALUE rotation, const TVALUE x, const TVALUE y)
 	{
 		return getTranslationMatrix<TVALUE>(-x, -y) * getRotationMatrix<TVALUE>(rotation) * getTranslationMatrix<TVALUE>(x, y);
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getRotationMatrix(const TVALUE rotation, const Common::Vector2<TVALUE>& plot)
+	Common::Matrix3<TVALUE> getRotation2dMatrix(const TVALUE rotation, const Common::Vector2<TVALUE>& plot)
 	{
 		return getTranslationMatrix<TVALUE>(-plot.Values[0], -plot.Values[1]) * getRotationMatrix<TVALUE>(rotation) * getTranslationMatrix<TVALUE>(plot.Values[0], plot.Values[1]);
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getReflectionMatrixAxisX()
+	Common::Matrix3<TVALUE> getReflection2dMatrixAxisX()
 	{
 		return Common::Matrix3<TVALUE>(  1.0,  0.0,  0.0,
 										 0.0, -1.0,  0.0,
@@ -59,7 +59,7 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getReflectionMatrixAxisY()
+	Common::Matrix3<TVALUE> getReflection2dMatrixAxisY()
 	{
 		return Common::Matrix3<TVALUE>( -1.0,  0.0,  0.0,
 										 0.0,  1.0,  0.0,
@@ -67,7 +67,7 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getReflectionMatrixAxisXY()
+	Common::Matrix3<TVALUE> getReflection2dMatrixAxisXY()
 	{
 		return Common::Matrix3<TVALUE>(  0.0,  1.0,  0.0,
 										 1.0,  0.0,  0.0,
@@ -75,7 +75,7 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getReflectionMatrixAxisYX()
+	Common::Matrix3<TVALUE> getReflection2dMatrixAxisYX()
 	{
 		return Common::Matrix3<TVALUE>(  0.0, -1.0,  0.0,
 										-1.0,  0.0,  0.0,
@@ -83,7 +83,7 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getSheerMatrix(const TVALUE x, const TVALUE y)
+	Common::Matrix3<TVALUE> getSheer2dMatrix(const TVALUE x, const TVALUE y)
 	{
 		return Common::Matrix3<TVALUE>(  1.0,    x,  0.0,
 										   y,  1.0,  0.0,
@@ -91,13 +91,13 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getSheerMatrix(const TVALUE x, const TVALUE y, const Common::Vector2<TVALUE>& plot)
+	Common::Matrix3<TVALUE> getSheer2dMatrix(const TVALUE x, const TVALUE y, const Common::Vector2<TVALUE>& plot)
 	{
 		return getTranslationMatrix<TVALUE>(-plot.Values[0], -plot.Values[1]) * getSheerMatrix<TVALUE>(x, y) * getTranslationMatrix<TVALUE>(plot.Values[0], plot.Values[1]);
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getSheerMatrixAxisX(const TVALUE x)
+	Common::Matrix3<TVALUE> getSheer2dMatrixAxisX(const TVALUE x)
 	{
 		return Common::Matrix3<TVALUE>(  1.0,    x,  0.0,
 										 0.0,  1.0,  0.0,
@@ -105,13 +105,13 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getSheerMatrixAxisX(const TVALUE x, const Common::Vector2<TVALUE>& plot)
+	Common::Matrix3<TVALUE> getSheer2dMatrixAxisX(const TVALUE x, const Common::Vector2<TVALUE>& plot)
 	{
 		return getTranslationMatrix<TVALUE>(-plot.Values[0], -plot.Values[1]) * getSheerMatrixAxisX<TVALUE>(x) * getTranslationMatrix<TVALUE>(plot.Values[0], plot.Values[1]);
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getSheerMatrixAxisY(const TVALUE y)
+	Common::Matrix3<TVALUE> getSheer2dMatrixAxisY(const TVALUE y)
 	{
 		return Common::Matrix3<TVALUE>(  1.0,  0.0,  0.0,
 										   y,  1.0,  0.0,
@@ -119,7 +119,7 @@ namespace Space2d
 	}
 
 	template <typename TVALUE>
-	Common::Matrix3<TVALUE> getSheerMatrixAxisY(const TVALUE y, const Common::Vector2<TVALUE>& plot)
+	Common::Matrix3<TVALUE> getSheer2dMatrixAxisY(const TVALUE y, const Common::Vector2<TVALUE>& plot)
 	{
 		return getTranslationMatrix<TVALUE>(-plot.Values[0], -plot.Values[1]) * getSheerMatrixAxisY<TVALUE>(y) * getTranslationMatrix<TVALUE>(plot.Values[0], plot.Values[1]);
 	}
